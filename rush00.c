d3ec4decd7b4dba0dff68288bd84f6acdc56a62f
asmaa-aljazzar <asma.jazar.319@gmail.com>
2024-06-07 17:31:58 -0400

    Upload rush00 file

---
+++ rush00.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+
+void ft_putchar(char c);
+
+void	rush(int x, int y)
+{
+	int row,col;
+	row = 1;
+	if (x >= 0 && y >= 0)
+	{
+		while(row <= y)
+		{
+			col = 1;
+			while(col <= x)
+			{
+				if ( (row == 1 && col ==1) || ( row == y && col == x)  || (row == y && col ==1) || (row == 1 && col == x ))
+				{
+					ft_putchar('o');
+				}
+				else if ( row == 1 || row == y)
+				{
+					ft_putchar('-');
+				}
+				else if (col == 1 || col == x )
+				{
+					ft_putchar('|');
+				}
+				else
+				{
+					ft_putchar(' ');
+				}
+				col++;
+			}
+			ft_putchar('\n');
+			row++;		
+		}
+	}
+	else
+	{
+		write(1, "Please Enter Just Positive values", 30);
+	}
+}
